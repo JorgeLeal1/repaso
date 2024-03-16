60b300a667006effbb5260c2c92d27176dd196c1
JorgeLeal <leal.jorge05@gmail.com>
2024-03-16 11:51:22 -0300

    esta es la primera carga

---
+++ assets/js/login.js
@@ -0,0 +1,38 @@
+/* Jquery*/
+
+
+
+function verValorInput() {
+    const valorEscrito = $("#inputUsuario").val();
+    alert($("$inputUsuario").val());
+
+    $("#inputUsuario").val(valorEscrito + " Agregado desde java");
+}
+
+
+
+
+
+/*
+Javascript
+
+const labelUsuario = document.getElementById("labelUsuario");
+const btnIngresar = document.getElementById("btnIngresar");
+
+labelUsuario.innerText ="LABEL";
+labelUsuario.style.color = "#f20808";
+
+
+// Funcion anonima 
+btnIngresar.addEventListener("click", ()=>{
+    //alert("Click");
+    verValorInput();
+
+});
+
+function verValorInput(){
+    const inputUsuario = document.getElementById("inputusuario");
+    alert(inputUsuario.value)
+}
+
+*/
